cef98e8bf10726addc160d896ef787340f3f9be4
Hans <coolguazi@gmail.com>
2022-12-12 20:13:15 +0800

    finish

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
-# Wordle Solver
+# Wordle Solver AI
+### Version 1.0.0
+
 ### Brief
-I create a solver for pop game WORDLE. With this program, you can:
+I create a solver featuring AI for the pop game WORDLE. With this program, you can:
 1. Just give it a hint each round when it guesses a word, and you'll be able to solve puzzles quickly.
-1. Extend the corpus from your word list file if needed. (Tip: Infrequently used words may lead to bad performance.)
+
+1. Extend the corpus from your word list file if needed. 
+
+### Prerequisite
+To run the code correctly, you need to install *scikit-learn* and *numpy* packages using
+
+`pip install -U scikit-learn`
+
+`pip install numpy`
 
 ### Usage
 Enter the folder where `main.py` is located, and
@@ -14,12 +24,14 @@ Enter the folder where `main.py` is located, and
 * extend the corpus and run the solver with
 
     ```python main.py [-u <path>][--update <path>]```
+
+(Tip: Infrequently used words may lead to bad performance.)
     
 
 ### Interactions
-In each round, the solver will first guess a word. At this time, you have to give it a **hint** according to the result (the default is green, yellow and gray spots), and proceed in this way until the end of the game. E.g: If the result shows like this, 
+First and foremost, you can decide whether to use AI features or not. If yes, the solver will use it to guess the word. In each round, the solver will first guess a word. At this time, you have to give it a **hint** according to the result (the default is green, yellow and gray spots), and proceed in this way until the end of the game. E.g: If the result shows like this, 
 
-![](https://github.com/coolguazitech/WordleSolver/blob/main/wordle_eg.jpg)
+![](https://github.com/coolguazitech/WordleSolver/blob/main/assets/wordle_eg.jpg)
 
 then the hint you'll give it is 'XBXXA', where 'X' stands for gray, 'A' for green, and 'B' for yellow.
 If the result shows there is no such word, enter one '!'. In the case the chances will not decrement.
--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from sys import argv
 from collections import Counter
-from random import shuffle
+import os
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+import numpy as np
+from numpy.random import normal
 
-CHANCES = 6
+VERSION = '1.0.0'
+
+N_CHANCES = 6
 WORD_LENGTH = 5
+RATING_RANGE = 3
 VOWELS = {'a', 'e', 'i', 'o', 'u'}
 
 def extend_corpus(src_path, dst_path='corpus.txt'):
@@ -17,12 +25,60 @@ def extend_corpus(src_path, dst_path='corpus.txt'):
     src_f.close()
     dst_f.close()
 
+def add_stop_word(word, dst_path='stop_words.txt'):
+    """Add a stop word."""
+    with open(dst_path, 'a') as f:
+        f.write(word + '\n')
+
+def insert_results(results, dst_path=os.path.join('ml', 'guess_results_5.data')):
+    """Insert game results into archive for online learning.
+
+    Parameters
+    ----------
+    results : 2D array, where nRow, nCol = (USED CHANCES, WORD_LENGTH * 2)
+      Each row would be [g1, g2, ..., gk, r1, r2, ..., rk] where gi denotes the guessed letter;
+      ri denotes the result letter, and k denotes WORD_LENGTH.
+
+    """
+    m, n = len(results), len(results[0]) // 2
+    results_with_scores = _score(results)
+    dst_f = open(dst_path, 'a')
+    for r in results_with_scores:
+        dst_f.write(','.join(map(str, r)) + '\n')
+    dst_f.close()
+
+def _score(results):
+    """Define scoring criteria for guess results.
+
+    Parameters
+    ----------
+    results : 2D array, where nRow, nCol = (USED CHANCES, WORD_LENGTH * 2)
+      Each row would be [g1, g2, ..., gk, r1, r2, ..., rk] where gi denotes the guessed letter;
+      ri denotes the result letter, and k denotes WORD_LENGTH.
+
+    Returns
+    -------
+    results_with_scores : 2D array, where nRow, nCol = (USED CHANCES, WORD_LENGTH + 1)
+      Each row would be [n1, n2, ..., nk, score] where ni denotes the ASCII code of gi,
+      k denotes WORD_LENGTH, and the score demonstrates how good this guess is.
+
+    """
+    m, n = len(results), len(results[0]) // 2
+    results_with_scores = [list(map(ord, r[:n])) + [0] for r in results]
+
+    # Current criterion:
+    char_2_point = {'A': 2, 'B': 1, 'X': 0}
+    for i, r in enumerate(results):
+        cnt = Counter(r[n:])
+        results_with_scores[i][-1] = (N_CHANCES - i) * sum(char_2_point[c] * cnt[c] for c in cnt)
+
+    return results_with_scores
+
 class Wordlist:
-    def __init__(self, word_length=5):
+    def __init__(self, enable_AI=False):
         self._size = 0
         self._words = []
-        self.word_length = word_length
-        self.criterion = lambda w: sum(c in VOWELS for c in w)
+        self.enable_AI = enable_AI
 
     @property
     def size(self):
@@ -33,32 +89,89 @@ class Wordlist:
         return self._words
     
     def rearrange_words(self):
-        """Rearrange words by criterion."""
-        shuffle(self._words)
-        self._words.sort(key=self.criterion)
+        """Sort by the sorting criteria."""
+        if self.enable_AI:
+            self.estimator = self._get_learned_regressor()
+        self._words.sort(key=self._sort_criteria)
         
-    def load_words(self, src_path='corpus.txt'):
-        """Load words with its size from corpus."""
-        words = set()
-        with open(src_path, 'r') as f:
-            while True:
-                w = f.readline()
-                if not w: break
-                w = w.strip().lower()
-                if w.isalpha() and len(w) == self.word_length:
-                    words.add(w)
+    def load_words(self, src_path='corpus.txt', stop_words_path='stop_words.txt'):
+        """Load and count the valid words from corpus except for stop words."""
+        print('Load words from corpus...')
+        words, stop_words = set(), set()
+        words_f = open(src_path, 'r')
+        stop_words_f = open(stop_words_path, 'r')
+
+        while True:
+            w = stop_words_f.readline()
+            if not w: break
+            w = w.strip().lower()
+            if w.isalpha() and len(w) == WORD_LENGTH:
+                stop_words.add(w)
+
+        while True:
+            w = words_f.readline()
+            if not w: break
+            w = w.strip().lower()
+            if w.isalpha() and len(w) == WORD_LENGTH and w not in stop_words:
+                words.add(w)
+
+        words_f.close()
+        stop_words_f.close()
         self._size, self._words = len(words), list(words)
 
+    def _sort_criteria(self, word):
+        """Determine how words are sorted.
+
+        Retruns
+        -------
+        priority : float
+          a number randomized with Gaussian noise 
+
+        """
+        # 1 to RATING_RANGE points for each criterion
+        priority = 0
+
+        # Criterion 1: the number of vowels in the word
+        priority += int(sum(c in VOWELS for c in word) * (RATING_RANGE - 1) / WORD_LENGTH) + 1
+
+        # Criterion 2: the diversity of letters in the word
+        priority += int((len(set(word)) - 1) * (RATING_RANGE - 1) / (WORD_LENGTH - 1)) + 1
+
+        # Criterion 3: the AI scorer
+        if self.enable_AI:
+            maximum = 2 * N_CHANCES * WORD_LENGTH
+            _word = list(map(ord, word))
+            priority += int(self.estimator.predict([_word])[0] * (RATING_RANGE - 1) / maximum) + 1
+
+        return priority * normal(loc=1.0, scale=0.1)
+
+    def _get_learned_regressor(self, src_path=os.path.join('ml', 'guess_results_5.data')):
+        """Load the data and use it to train a regression model, then return the model."""
+        # load data
+        src_f = open(src_path, 'r')
+        pipe_lr = Pipeline([('ss', StandardScaler()), ('lr', LinearRegression())])
+        data = [r[:-1].split(',') for r in src_f.readlines()]
+        src_f.close()
+
+        # train model
+        data = np.array(data, dtype=np.float)
+        X, y = data[:, :-1], data[:, -1]
+        pipe_lr.fit(X, y)
+
+        return pipe_lr
+
 class Solver:
-    def __init__(self, wordlist, word_length=5):
-        self.wordlist = wordlist
-        self.size = wordlist.size
-        self.words = wordlist.words
-        self.word_length = word_length
+    def __init__(self, enable_AI=False):
+        self.wordlist = Wordlist(enable_AI)
+        self.wordlist.load_words()
+        self.wordlist.rearrange_words()
+        self.size = self.wordlist.size
+        self.words = self.wordlist.words
+        self.enable_AI = enable_AI
         
     def reset(self):
         """Reset the solver."""
-        self.__init__(self.wordlist)
+        self.__init__(self.enable_AI)
         
     def next_word(self):
         """Pop out the next possible word."""
@@ -70,13 +183,13 @@ class Solver:
     def _judge(self, guess, ans):
         """Determine how the guess and the answer match."""
         cnt = Counter(ans)
-        judgment = ['X'] * self.word_length
-        for i in range(self.word_length):
+        judgment = ['X'] * WORD_LENGTH
+        for i in range(WORD_LENGTH):
             if guess[i] == ans[i]: 
                 judgment[i] = 'A'
                 cnt[guess[i]] -= 1
 
-        for i in range(self.word_length):
+        for i in range(WORD_LENGTH):
             if cnt[guess[i]]: 
                 judgment[i] = 'B'
                 cnt[guess[i]] -= 1
@@ -93,31 +206,44 @@ class Solver:
         self.words = remained_words
 
 if __name__ == '__main__':
+    # initialize corpus
     if len(argv) != 1 and len(argv) != 3 or len(argv) == 3 and not (argv[1] == '-u' or argv[1] == '--update'):
         raise Exception('Provide no arguments and start solver. Or, use "[-u <path>][--update <path>]" to extend the corpus.')
     elif len(argv) != 1:
         print('Update the corpus...')
         extend_corpus(argv[2])
-    print('Load words from corpus...')
-    wl = Wordlist(WORD_LENGTH)
-    wl.load_words()
-    wl.rearrange_words()
-    solver = Solver(wl, WORD_LENGTH)
+    
+    # check if the AI features are enabled
+    enable_AI = False
+    ans = input('Do you want to enable AI features? [y/n] ')
+    if ans.lower() == 'y':
+        enable_AI = True
+
+    # initialize the solver
+    solver = Solver(enable_AI=enable_AI)
     print('Start!')
 
-    for _ in range(CHANCES):
+    results = []
+    for _ in range(N_CHANCES):
         while True:
             guess = solver.next_word()
             while True:
                 inputs = input(f'I guess the word is "{guess}". Please give me the hint ("ABBAX" or "!"): ').upper()
-                if inputs == '!' or len(inputs) == WORD_LENGTH and inputs.isalpha() and set(inputs) <= set("ABX") : break
+                if inputs == '!' or len(inputs) == WORD_LENGTH and inputs.isalpha() and set(inputs) <= set("ABX"): 
+                    break
                 print('Invalid inputs, please try again.')
-            if inputs != '!': break
-
+            if inputs == '!': 
+                add_stop_word(guess)
+                continue
+            else: break
+                
+        results.append([*guess, *inputs])
         if inputs == 'A' * WORD_LENGTH:
             print('You win!')
             break
 
         solver.update_words(guess, inputs)
     else: print('Sorry I can\'t solve it...')
+    insert_results(results)
+    print('Thank you for using! We expect your feedback.')
             
